470e6da6292c8f0d103afc6207fe1eefc1177dac
Sravani Mulagundla <sravani.mulagundla@syenainfosoft.com>
2026-02-10 02:12:38 +0530

    added photos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { GalleryComponent } from "./components/gallery/gallery.component";
 import { StoryComponent } from "./components/story/story.component";
 import { LoveLetterComponent } from "./components/love-letter/love-letter.component";
 import { WeddingComponent } from "./wedding/wedding.component";
+import { AboutComponent } from "./about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent, GalleryComponent, StoryComponent, LoveLetterComponent, WeddingComponent],
+  imports: [HeroComponent, GalleryComponent, StoryComponent, LoveLetterComponent, WeddingComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -9,5 +9,33 @@ import { CommonModule, NgFor } from '@angular/common';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent {
+months = [
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+  'January'
+];
+memories = [
+  { month: 'February', text: 'Shivaratri' },
+  { month: 'March', text: '16 rojulla panduga' },
+  { month: 'April', text: 'Ugadi' },
+  { month: 'May', text: 'Naris Birthday' },
+  { month: 'June', text: 'Going to Relakayapally' },
+  { month: 'July', text: 'Leonia Resort' },
+  { month: 'August', text: 'Tirupathi' },
+  { month: 'September', text: 'Vinayaka Chavithi' },
+  { month: 'October', text: 'Gandipet Landscape' },
+  { month: 'November', text: 'Swarnagiri' },
+  { month: 'December', text: 'Sravs Birthday' },
+  { month: 'January', text: 'Medaram' }
+];
 
 }
