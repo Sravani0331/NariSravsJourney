9437abb6fb78fddf21297ff5a9e8e4368fb1cbb8
Sravani Mulagundla <sravani.mulagundla@syenainfosoft.com>
2026-02-12 00:52:25 +0530

    updated code.

--- src/app/wedding/wedding.component.ts
+++ src/app/wedding/wedding.component.ts
@@ -3,10 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-wedding',
   standalone: true,
-  imports: [],
   templateUrl: './wedding.component.html',
-  styleUrl: './wedding.component.scss'
+  styleUrls: ['./wedding.component.scss']
 })
 export class WeddingComponent {
+  joined = false;
+  showFinal = false;
+  timer: any;
 
+  toggleJoin() {
+
+    // 🔁 RESET (Second click)
+    if (this.joined && this.showFinal) {
+      clearTimeout(this.timer);
+
+      this.joined = false;
+      this.showFinal = false;
+      return;
+    }
+
+    // ▶️ FIRST CLICK (Start animation)
+    if (!this.joined) {
+      this.joined = true;
+
+      // 6s move + 2s pause = 8s
+      this.timer = setTimeout(() => {
+        this.showFinal = true;
+      }, 4000);
+    }
+  }
 }
