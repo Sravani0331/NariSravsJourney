2182b930b07e335a1bc28662a8d104ebbdb18362
Sravani Mulagundla <sravani.mulagundla@syenainfosoft.com>
2026-02-13 00:11:58 +0530

    updated code.

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,13 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, AfterViewInit } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [DialogModule, CommonModule],
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss']
 })
 export class HeroComponent implements AfterViewInit {
+  showLoveDialog = false;
+openLovePopup() {
+    this.showLoveDialog = true;
+    const music = document.getElementById('bgMusic') as HTMLAudioElement;
+    music?.play();
+  }
   ngAfterViewInit() {
     const music = document.getElementById('bgMusic') as HTMLAudioElement;
     const btn = document.getElementById('musicToggle') as HTMLElement;
