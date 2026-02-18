72355766041af2f62ce958d6fb874419869d5133
Sravani Mulagundla <sravani.mulagundla@syenainfosoft.com>
2026-02-18 18:52:38 +0530

    updated headers.

--- src/app/wedding/wedding.component.ts
+++ src/app/wedding/wedding.component.ts
@@ -8,24 +8,17 @@ import { Component } from '@angular/core';
 })
 export class WeddingComponent {
   joined = false;
-  showFinal = false;
-  timer: any;
+showFinal = false;
+timer: any;
 
-  toggleJoin() {
-    if (this.joined && this.showFinal) {
-      clearTimeout(this.timer);
-      this.joined = false;
-      this.showFinal = false;
-      return;
-    }
+toggleJoin() {
+  if (this.joined) return; 
 
-    // ▶️ FIRST CLICK (Start animation)
-    if (!this.joined) {
-      this.joined = true;
-      // 6s move + 2s pause = 8s
-      this.timer = setTimeout(() => {
-        this.showFinal = true;
-      }, 4000);
-    }
-  }
+  this.joined = true;
+
+  this.timer = setTimeout(() => {
+    this.showFinal = true;
+  }, 4000);
+}
+
 }
